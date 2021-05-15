5faf8be4dde1757afec42ae98528152693fe993e
Gabriel <gleandro0795@gmail.com>
2021-05-14 22:02:42 -0300

    Create the project Ia Robot

---
+++ Assets/AI.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+    // Obtem a posição do player
+    public Transform player;   
+
+    //Obtem a posição do tiro(Bullet)
+    public Transform bulletSpawn;
+
+    //Obtem o status do slide no game
+    public Slider healthBar;  
+    
+    //Referencia do tiro como objeto
+    public GameObject bulletPrefab;
+
+    //Acessa o navmesh do objeto
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+
+    //Variavel para vida
+    float health = 100.0f;   
+    
+    //Variavel para a rotação no nav Mesh
+    float rotSpeed = 5.0f;
+
+    float visibleRange = 80.0f;
+    float shotRange = 40.0f;
+
+    void Start()
+    {
+        //Inicialização do navMesh
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        //Metodo para exibir status da vida
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+    void Update()
+    {
+        //Exibi a barra de vida sobre o objeto
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        healthBar.value = (int)health;
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+        //Metodo referente a vida (Regenerar)
+       if(health < 100)
+        health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Metodo ao colidir com objeto com a tag bullet remove vida
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+    }
+
+    [Task]
+    public void PickRandomDestination()
+    {
+        //Metodo para realizar o uso do plugin Pand para navegação aleatoria
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void MoveToDestination()
+    {
+        //Metodo para realizar o deslocamento com o plugin Panda
+        if(Task.isInspected)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        if(agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+    }
+}
+
---
+++ Assets/Drive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+    //Variavel relacionada a velocidade
+	float speed = 20.0F;
+    //Variavel de rotação
+    float rotationSpeed = 120.0F;
+
+    //Obtem o objeto do bullet
+    public GameObject bulletPrefab;
+    //obtem a posição do bullet
+    public Transform bulletSpawn;
+
+    void Update() {
+        //Variaveis de deslocamento utilizando inputs pre configurados pela unity
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        //Deslocamento do player
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //Ao pressionar espaço realiza o instanciamento de um bullet 
+        if(Input.GetKeyDown("space"))
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+    }
+}
